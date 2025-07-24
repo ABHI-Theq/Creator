75cc5cd4493eed47d8a3e97f00a35c7fa4736007
ABHI-Theq <abhi03085e@gmail.com>
2025-07-24 23:00:12 +0530

    e2b sandboxes setup added

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,9 +1,15 @@
 import { inngest } from "./client";
+import {Sandbox} from "@e2b/code-interpreter";
 import { Agent, openai, createAgent, gemini } from "@inngest/agent-kit";
+import { getsandboxUrl } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event,step }) => {
+    const sandboxId=await step.run('get-sandbox-Id',async()=>{
+      const sandbox=await Sandbox.create('creator-nextjs-t-2')
+      return sandbox.sandboxId
+    })
 
       const codeAgent = createAgent({
       name: "code-agent",
@@ -15,8 +21,13 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}`,
     );
 
+    const sandboxUrl=await step.run('get-sandbox-url',async ()=>{
+      const sandbox=await getsandboxUrl(sandboxId)
+      return `https://${sandbox.getHost(3000)}`;
+    })
 
 
-    return { output};
+
+    return { output,sandboxUrl};
   },
 );
